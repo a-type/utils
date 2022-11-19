142b371ab392a69b1f6002412b788cbeccfd2bd5
Grant Forrest <gaforres@gmail.com>
2022-11-19 14:50:07 -0500

    add debounce

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,19 @@ export function assert(
 		throw new Error(message);
 	}
 }
+
+export function debounce<Fn extends (...args: any[]) => void>(
+	callback: Fn,
+	delay: number,
+): Fn {
+	let timeout: NodeJS.Timeout | undefined;
+	return ((...args) => {
+		if (timeout !== undefined) {
+			clearTimeout(timeout);
+		}
+		timeout = setTimeout(() => {
+			timeout = undefined;
+			callback(...args);
+		}, delay);
+	}) as Fn;
+}
