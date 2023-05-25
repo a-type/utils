878beb2406a0cf62b8272f682045aca84057cdb9
Grant Forrest <gaforres@gmail.com>
2023-05-25 16:32:55 -0400

    add various string utils, restructure

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,3 @@
-export function assert(
-	condition: any,
-	message: string = 'assertion failed',
-): asserts condition {
-	if (!condition) {
-		throw new Error(message);
-	}
-}
-
-export function debounce<Fn extends (...args: any[]) => void>(
-	callback: Fn,
-	delay: number,
-): Fn {
-	let timeout: NodeJS.Timeout | undefined;
-	return ((...args) => {
-		if (timeout !== undefined) {
-			clearTimeout(timeout);
-		}
-		timeout = setTimeout(() => {
-			timeout = undefined;
-			callback(...args);
-		}, delay);
-	}) as Fn;
-}
+export * from './assert.js';
+export * from './timing.js';
+export * from './strings.js';
