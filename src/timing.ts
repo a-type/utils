aaa09368bd3af6282338ebe82eb05851a215818c
Grant Forrest <gaforres@gmail.com>
2024-05-30 11:40:01 -0400

    add throttle

--- src/timing.ts
+++ src/timing.ts
@@ -13,3 +13,20 @@ export function debounce<Fn extends (...args: any[]) => void>(
     }, delay);
   }) as Fn;
 }
+
+export function throttle<Fn extends (...args: any[]) => void>(
+  callback: Fn,
+  delay: number,
+): Fn {
+  let lastTime = 0;
+  return ((...args) => {
+    const now = Date.now();
+    // makes sure the latest value is always sent after throttling ends
+    const finalizer = debounce(callback, delay);
+    if (now - lastTime >= delay) {
+      lastTime = now;
+      callback(...args);
+    }
+    finalizer(...args);
+  }) as Fn;
+}
