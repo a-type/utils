afded140cc588c65e6417f404bda0be34775c7b0
Grant Forrest <gaforres@gmail.com>
2024-05-30 14:28:51 -0400

    mouse event utils

---
+++ .changeset/giant-toys-burn.md
@@ -0,0 +1,5 @@
+---
+"@a-type/utils": patch
+---
+
+mouse event utils
--- src/dom.ts
+++ src/dom.ts
@@ -5,3 +5,15 @@ export function stopPropagation(ev: any) {
 export function preventDefault(ev: any) {
   ev?.preventDefault?.();
 }
+
+export function isRightClick(ev: { button: number }) {
+  return ev.button === 2;
+}
+
+export function isLeftClick(ev: { button: number }) {
+  return ev.button === 0;
+}
+
+export function isMiddleClick(ev: { button: number }) {
+  return ev.button === 1;
+}
